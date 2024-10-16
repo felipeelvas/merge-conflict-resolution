bce6bf1408b9d8111367aefb75b3c70686506b60
Felipe Elvas Barbosa <felipeelvas@gmail.com>
2024-10-16 10:25:52 -0300

    Alterando local das funções

--- javascript-git-test/index.js
+++ javascript-git-test/index.js
@@ -3,7 +3,7 @@ const soma = (a, b ) => a + b;
 const multiplica = (a, b) => a * b;
 
 const main = () => {
-console.log(somaUmMaisUm())
-console.log(multiplicaDoisVezesUm())
+console.log(soma())
+console.log(multiplica())
 }
 main()
