c367b918d7fbf9cdd533cd36d7e0915d77522b14
Felipe Elvas Barbosa <felipeelvas@gmail.com>
2024-10-16 11:10:26 -0300

    alteração versio-b

--- javascript-git-test/index.js
+++ javascript-git-test/index.js
@@ -1,9 +1,9 @@
 const soma = (a, b ) => a + b;
 
 const multiplica = (a, b) => a * b;
-
+// aqui esta outra alteração
 const main = () => {
 console.log(soma())
-console.log(multiplica())
+console.log(multiplica(2,3))
 }
 main()
